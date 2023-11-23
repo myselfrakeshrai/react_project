ade62ac9702c76f679b4935f99d637fa569e5637
myselfrakeshrai <myselfrakeshrai@gmail.com>
2023-11-23 14:37:37 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const AboutJohn = (props) => {
@@ -19,29 +19,75 @@ const AboutJohn = (props) => {
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You have change the value!" + counter);
+  }, [counter]);
   return (
     <>
-      <button onClick={()=> setCounter((prevCount)=>prevCount -1)}>-</button>
-        <h1>{counter}</h1>
-      <button onClick={() => setCounter((addCount)=>addCount +1)}>+</button>
+      <h1> Counter Number</h1>
+      <p>This is the code to count the number.</p>
+      <h1 className="App-Count-h1">{counter}</h1>
+      <div className="Button-background">
+      <button
+        className="Button-count-danger Button-margin"
+        onClick={() => setCounter((prevCount) => prevCount - 1)}
+      >
+        -
+      </button>
+      <button
+        className="Button-count-primary Button-margin"
+        onClick={() => setCounter((addCount) => addCount + 1)}
+      >
+        +
+      </button>
+      </div>
+
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   const name = "Rakesh Rai";
   const isName = true;
   return (
-    <div className="App">
-      <Counter/>
-      <h1>Hi! I'm {isName ? name : "there is no name"}</h1>
-      <AboutJohn
-        name={"Rakesh"}
-        LastName={"Rai"}
-        age={2 + 2}
-        address={"Dharan"}
-      />
-    </div>
+    <>
+      <div className="App-row">
+        <div className="App">
+          <h1>Hi! I'm {isName ? name : "there is no name"}</h1>
+          <AboutJohn
+            name={"Rakesh"}
+            LastName={"Rai"}
+            age={2 + 2}
+            address={"Dharan"}
+          />
+          <div className="App-inner">
+            Myself {name} just a test to learn more about this code
+            <p> Here we go with the new forces and deal with the custom deals just a text to fill up the text and test is this really working.</p> 
+          </div>
+        </div>
+        <div className="App-1">
+          <Counter />
+        </div>
+      </div>
+      <div className="App-row">
+      <div className="Dash-1">
+          <p className="h1-dash">Custom Dash</p>
+          <p className="P-dash"> Just a custom dash to look at the works how is it working and just to look at the gradeient screen how it looks</p>
+        </div>
+        <div className="Dash-2">
+        <p className="h1-dash">Custom Dash</p>
+          <p className="P-dash"> Just a custom dash to look at the works how is it working and just to look at the gradeient screen how it looks</p>
+        </div>
+        <div className="Dash-3">
+        <p className="h1-dash">Custom Dash</p>
+          <p className="P-dash"> Just a custom dash to look at the works how is it working and just to look at the gradeient screen how it looks</p>
+        </div>
+      </div>
+      <div className="footer">
+        <p>copyright : Rakesh Rai</p>
+      </div>
+    </>
   );
 };
 
